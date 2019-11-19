7b99d2f5c35b2f79831f1de654a7a2c28df7bb78
ayoubechtaibi <ayoub.echtaibi@outlook.com>
2019-11-19 18:05:36 +0100

    some changes

--- app/Http/Controllers/HistoriqueController.php
+++ app/Http/Controllers/HistoriqueController.php
@@ -7,14 +7,14 @@ use Illuminate\Http\Request;
 
 class HistoriqueController extends Controller
 {
-    public function getDonnation($id){
+    public function getDonnation(){
         $user = auth()->user();
-        $clotured = Annonce::all()
-            ->where('status',0)
-            ->where('user_id',$user->id);
-        $non_clotured = Annonce::all()
-            ->where('status',1)
-            ->where('user_id',$user->id);
+        $clotured = Annonce::
+            where('status',0)
+            ->where('user_id',$user->id)->get();
+        $non_clotured = Annonce::
+            where('status',1)
+            ->where('user_id',$user->id)->get();
         return response()->json(['clotured'=>$clotured,'non_clotured'=>$non_clotured],200);
     }
 }
