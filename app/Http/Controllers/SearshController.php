e97d127b2df374792fbca7b8de221c2456aaf893
ouailmj <ouailmjahedooo@gmail.com>
2019-11-19 16:59:31 +0100

    seach fix

--- app/Http/Controllers/SearshController.php
+++ app/Http/Controllers/SearshController.php
@@ -12,25 +12,7 @@ class SearshController extends Controller
         $this->request = $request->all();
         $annonces = Annonce::where('status',1)->where(function ($q){
             $query = $this->request['query'];
-            $idCat = $this->request['categorie_id'];
-            $idFilter = $this->request['filter'];
-            $date = new \DateTime();
-            $formatted_date = '';
-            if($idFilter==1){
-                $date->modify('-24 hours');
-                $formatted_date = $date->format('Y-m-d H:i:s');
-            }else if($idFilter==2){
-                $date->modify('-7 day');
-                $formatted_date = $date->format('Y-m-d H:i:s');
-            }else if($idFilter==3){
-                $date->modify('-1 month');
-                $formatted_date = $date->format('Y-m-d H:i:s');
-            }
-            if($idCat==0){
-                $q->where('annonces.created_at','>',$formatted_date)->orwhere('title','like','%'.$query.'%')->orWhere('description','like','%'.$query.'%');
-            }else{
-                $q->where('annonces.created_at','>',$formatted_date)->where('categorie_id',$idCat)->where('title','like','%'.$query.'%')->orWhere('description','like','%'.$query.'%');
-            }
+                $q->orwhere('title','like','%'.$query.'%')->orWhere('description','like','%'.$query.'%');
         })
             ->leftJoin('images', function ($join){
                 $join->on('annonces.id', '=', 'images.annonce_id');
